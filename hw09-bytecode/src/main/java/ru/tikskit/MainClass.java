c00d5f774f5bb000c4f27807636e1186b6fd32a3
tikskit <tikskit@gmail.com>
2020-05-14 14:38:08 +0700

    Initial commit

---
+++ hw09-bytecode/src/main/java/ru/tikskit/MainClass.java
@@ -0,0 +1,15 @@
+package ru.tikskit;
+
+import ru.tikskit.business.Car;
+import ru.tikskit.business.Toyota;
+import ru.tikskit.instrumentation.ClassFactory;
+
+public class MainClass {
+    public static void main(String args[]) {
+        Car toyota = ClassFactory.createToyota();
+        toyota.meth1(10, 'a');
+        toyota.meth1(10, false);
+        toyota.meth2();
+
+    }
+}
---
+++ hw09-bytecode/src/main/java/ru/tikskit/business/Car.java
@@ -0,0 +1,7 @@
+package ru.tikskit.business;
+
+public interface Car {
+    void meth1(int p1, char p2);
+    void meth1(int p1, boolean p2);
+    void meth2();
+}
